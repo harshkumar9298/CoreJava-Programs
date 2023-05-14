720dd47101c15555c0f8c983e37877f78057a0d6
CWS <CWS@DESKTOP-QTNCLAJ>
2023-05-15 00:13:34 +0530

    commit;

---
+++ Inheritance_app4/src/maccess/DemoInheritance3.java
@@ -0,0 +1,7 @@
+package maccess;
+import test.*;
+public class DemoInheritance3 {
+	public static void main(String[] args) {
+		CClass ob = new CClass();//CClass_Con_Call
+	 }
+}
---
+++ Inheritance_app4/src/test/PClass.java
@@ -0,0 +1,6 @@
+package test;
+public class PClass {
+	public PClass() {
+		System.out.println("====PClass con====");
+	}
+}
