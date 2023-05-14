b6d839119c65a85058289f2d34cad8947995c691
CWS <CWS@DESKTOP-QTNCLAJ>
2023-05-15 00:17:25 +0530

    commit;

---
+++ InnerAbstractClass_app20(InnerAbstractClass in AbstractClass)/src/test/AClass.java
@@ -0,0 +1,15 @@
+package test;
+public abstract class AClass {
+	public abstract class AClass1{
+		 
+		public abstract void m1(int x);
+		
+	}
+	
+	public static abstract class AClass2{
+		 
+		public abstract void m2(int y);
+		
+		}
+
+}
