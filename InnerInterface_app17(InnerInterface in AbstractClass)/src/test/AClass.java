ff93be2dd3e7bc1dd0b58fdd277a7ddfcf187e29
CWS <CWS@DESKTOP-QTNCLAJ>
2023-05-15 23:04:53 +0530

    commit;

---
+++ InnerInterface_app17(InnerInterface in AbstractClass)/src/test/AClass.java
@@ -0,0 +1,13 @@
+package test;
+public abstract class AClass {
+	public interface ITest1{
+		public abstract void m1(int x);
+	}
+	
+	public static interface ITest2{
+		
+		public abstract void m2(int y);
+		
+	}
+
+}
